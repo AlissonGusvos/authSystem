281bfdfda9af5a0c4a2b89e2e291579a6fb3793d
Alisson Gonçalves <gustavoalisson322@gmail.com>
2026-02-25 09:44:12 -0300

    Entidade 'User' criada

---
+++ src/main/java/com/auth/authSystem/entity/User.java
@@ -0,0 +1,48 @@
+package com.auth.authSystem.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "login")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String user;
+    private String email;
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
