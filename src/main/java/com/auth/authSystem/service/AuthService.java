03778bd99d6042a16ef917cff61ca5b2ad4af41d
Alisson Gonçalves <gustavoalisson322@gmail.com>
2026-02-26 00:20:37 -0300

    Criptografia BCrypt

--- src/main/java/com/auth/authSystem/service/AuthService.java
+++ src/main/java/com/auth/authSystem/service/AuthService.java
@@ -3,6 +3,7 @@ package com.auth.authSystem.service;
 import com.auth.authSystem.entity.User;
 import com.auth.authSystem.repository.AuthRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,16 +14,28 @@ public class AuthService {
     @Autowired
     private AuthRepository authRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean checkPass(String senha, String senhaBanco){
+        return passwordEncoder.matches(senha, senhaBanco);
+    }
+
     public String register(User user) {
+        user.setSenha(passwordEncoder.encode(user.getSenha()));
         authRepository.save(user);
         return "Usuário cadastrado";
     }
 
     public String login(String user, String senha) {
-        Optional<User> reqUser = authRepository.findByUserAndSenha(user, senha);
+        Optional<User> reqUser = authRepository.findByUser(user);
 
         if (reqUser.isPresent()) {
-            return "Login feito com sucesso";
+            User userVerify = reqUser.get();
+
+            if (checkPass(senha, userVerify.getSenha())) {
+                return "Login feito com sucesso";
+            }
         }
         return "Acesso negado";
     }
